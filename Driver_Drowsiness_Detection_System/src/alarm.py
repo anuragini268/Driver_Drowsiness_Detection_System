0f4fce85bccbd5292070bd830e13c43709b1b1e5
anuragini268 <raginianu55@gmail.com>
2026-07-03 19:41:30 +0530

    Delete Driver_Drowsiness_Detection_System directory

--- Driver_Drowsiness_Detection_System/src/alarm.py
+++
@@ -1,7 +0,0 @@
-import winsound
-
-def play_alarm():
-    """
-    Plays the default Windows beep sound.
-    """
-    winsound.Beep(2500, 1000)   # Frequency = 2500 Hz, Duration = 1000 ms
--- Driver_Drowsiness_Detection_System/test_wav.py
+++
@@ -1,8 +0,0 @@
-import wave
-
-try:
-    wav = wave.open("sounds/alarm.wav", "rb")
-    print("WAV File is OK")
-    wav.close()
-except Exception as e:
-    print(e)
